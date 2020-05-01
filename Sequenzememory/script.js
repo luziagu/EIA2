6736c75cc14e975b03a6bf5a15625481880f98b6
luziagu <luzia.gunzenhauser@yahoo.de>
2020-05-01 18:25:48 +0200

    Neuer Versuch Script

--- Sequenzememory/script.js
+++ Sequenzememory/script.js
@@ -1,10 +1,26 @@
 console.log("verknüpft");
+var randomWord;
+var startGame;
 var sequences = ["Furtwangen", "Corona", "Entwicklung Interaktiver Anwedungen"];
-function chooseRandomWord(_Event) {
-    var randomWordButton = document.getElementById("randomWord");
-    randomWordButton.addEventListener("click", chooseWord);
-}
-function chooseWord() {
-    console.log("Ich wurde geklickt.");
+var enterWord = [];
+document.addEventListener("load", function () {
+    randomWord = document.querySelector("#randomWord");
+    startGame = document.querySelector("#startGame");
+    startGame.addEventListener("click", function () {
+        shufflecharacters();
+    });
+    randomWord.addEventListener("click", function () {
+        console.log("ich wurde geklickt");
+    });
+});
+function shufflecharacters() {
+    var tmp, rand;
+    for (var i = 0; i < sequences.length; i++) {
+        rand = Math.floor(Math.random() * sequences.length);
+        tmp = sequences[i];
+        sequences[i] = sequences[rand];
+        sequences[rand] = tmp;
+    }
+    console.log("gemischt");
 }
 //# sourceMappingURL=script.js.map
--- Sequenzememory/script.ts
+++ Sequenzememory/script.ts
@@ -1,15 +1,38 @@
+
 console.log("verknüpft"); 
 
+let randomWord: HTMLElement;
+let startGame: HTMLElement; 
 let sequences: string [] = ["Furtwangen", "Corona", "Entwicklung Interaktiver Anwedungen"];
+let enterWord: string [] = []; 
 
-function chooseRandomWord(_Event: Event): void {
-   let randomWordButton: HTMLElement = document.getElementById("randomWord"); 
-   randomWordButton.addEventListener("click", chooseWord);
+document.addEventListener("load", function(): void {
 
-}
+    randomWord = document.querySelector("#randomWord");
+    startGame = document.querySelector("#startGame"); 
 
-function chooseWord(): void {
- 
-    console.log("Ich wurde geklickt."); 
 
+    startGame.addEventListener("click", function(): void {
+        shufflecharacters();
+        
+    });
+
+    randomWord.addEventListener("click", function(): void {
+        
+        console.log("ich wurde geklickt"); 
+    });
+
+});
+
+
+
+function shufflecharacters(): void {
+    let tmp, rand;
+    for (var i: number = 0; i < sequences.length; i++) {
+      rand = Math.floor(Math.random() * sequences.length);
+      tmp = sequences[i]; 
+      sequences[i] = sequences[rand]; 
+      sequences[rand] = tmp;
+    }
+    console.log("gemischt");
 }
