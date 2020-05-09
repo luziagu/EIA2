935bed71cd565d56abc61ea88adec5e3eea1b3c3
luziagu <luzia.gunzenhauser@yahoo.de>
2020-05-09 11:58:57 +0200

    update

--- Formular/script.js
+++ Formular/script.js
@@ -1,2 +1,13 @@
 console.log("Verknüpft");
+var addButtonItem;
+var addButtonTask;
+window.addEventListener("load", function () {
+    addButtonItem = document.querySelector("#addItem");
+    addButtonTask = document.querySelector("#addTask");
+    addButtonItem.addEventListener("click", addNewRow);
+    addButtonTask.addEventListener("click", addNewRow);
+});
+function addNewRow() {
+    console.log("Ich wurde geklickt");
+}
 //# sourceMappingURL=script.js.map
--- Formular/script.ts
+++ Formular/script.ts
@@ -1,1 +1,24 @@
 console.log("Verknüpft"); 
+
+let addButtonItem: HTMLElement; 
+let addButtonTask: HTMLElement; 
+
+
+window.addEventListener("load", function(): void {
+
+    addButtonItem = document.querySelector("#addItem");
+    addButtonTask = document.querySelector("#addTask"); 
+     
+
+
+
+    addButtonItem.addEventListener("click", addNewRow);
+    addButtonTask.addEventListener("click", addNewRow );
+ 
+
+});
+
+function addNewRow (): void {
+
+console.log("Ich wurde geklickt"); 
+}
