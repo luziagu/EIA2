16192df8600392046a673ef577a81e5646302590
luziagu <luzia.gunzenhauser@yahoo.de>
2020-06-11 14:36:03 +0200

    Patterns

--- Corona-Canvas/corona.js
+++ Corona-Canvas/corona.js
@@ -2,17 +2,31 @@
 var coronaVirus;
 (function (coronaVirus) {
     console.log("load");
+    let canvas = document.querySelector("canvas");
+    let crc2 = canvas.getContext("2d");
     window.addEventListener("load", handleLoad);
-    let crc2;
     function handleLoad(_event) {
-        let canvas = document.querySelector("canvas");
-        if (!canvas)
-            return;
-        crc2 = canvas.getContext("2d");
         drawBackground();
     }
     function drawBackground() {
         console.log("Background");
+        let pattern = document.createElement("canvas").getContext("2d");
+        pattern.canvas.width = 40;
+        pattern.canvas.height = 420;
+        pattern.fillStyle = "pink";
+        pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+        pattern.moveTo(0, 20);
+        pattern.lineTo(20, 20);
+        pattern.lineTo(40, 0);
+        pattern.lineTo(50, 0);
+        pattern.lineTo(100, 20);
+        pattern.lineTo(50, 40);
+        pattern.lineTo(40, 40);
+        pattern.lineTo(20, 20);
+        pattern.stroke();
+        pattern.closePath();
+        crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
+        crc2.fillRect(0, 0, canvas.width, canvas.height);
     }
 })(coronaVirus || (coronaVirus = {}));
 //# sourceMappingURL=corona.js.map
--- Corona-Canvas/corona.ts
+++ Corona-Canvas/corona.ts
@@ -1,25 +1,45 @@
 namespace coronaVirus {
 
+    
     console.log("load");
     interface Vector {
         x: number;
         y: number;
     }
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+
     window.addEventListener("load", handleLoad);
-    let crc2: CanvasRenderingContext2D;
+
 
     function handleLoad(_event: Event): void {
-        let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
-        if (!canvas)
-            return;
-
-        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
+        
         drawBackground();
+
     }
 
     function drawBackground(): void {
         console.log("Background");
-        
+
+        let pattern: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.createElement("canvas").getContext("2d");
+        pattern.canvas.width = 40;
+        pattern.canvas.height = 420;
+        pattern.fillStyle = "pink";
+        pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+        pattern.moveTo(0, 20);
+        pattern.lineTo(20, 20);
+        pattern.lineTo(40, 0);
+        pattern.lineTo(50, 0);
+        pattern.lineTo(100, 20);
+        pattern.lineTo(50, 40);
+        pattern.lineTo(40, 40);
+        pattern.lineTo(20, 20);
+        pattern.stroke();
+        pattern.closePath();
+
+        crc2.fillStyle = <CanvasRenderingContext2D>crc2.createPattern(pattern.canvas, "repeat");
+        crc2.fillRect(0, 0, canvas.width, canvas.height);
     }
+
 
 }
