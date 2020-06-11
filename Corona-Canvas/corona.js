e84a8c3e13fd35f9ea42d38d5fc79c36e3fe16ae
luziagu <luzia.gunzenhauser@yahoo.de>
2020-06-11 14:55:47 +0200

    Pattern

--- Corona-Canvas/corona.js
+++ Corona-Canvas/corona.js
@@ -3,10 +3,15 @@ var coronaVirus;
 (function (coronaVirus) {
     console.log("load");
     let canvas = document.querySelector("canvas");
-    let crc2 = canvas.getContext("2d");
+    let crc2;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
         drawBackground();
+        drawHumanCell({ x: 350, y: 170 }, { x: 250, y: 250 });
     }
     function drawBackground() {
         console.log("Background");
@@ -27,6 +32,28 @@ var coronaVirus;
         pattern.closePath();
         crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
         crc2.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    function drawHumanCell(_position, _size) {
+        console.log("Cloud", _position, _size);
+        let nParticles = 30;
+        let radiusParticle = 60;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(70%, 130%, 180%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
     }
 })(coronaVirus || (coronaVirus = {}));
 //# sourceMappingURL=corona.js.map
--- Corona-Canvas/corona.ts
+++ Corona-Canvas/corona.ts
@@ -7,14 +7,20 @@ namespace coronaVirus {
         y: number;
     }
     let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
-    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+    let crc2: CanvasRenderingContext2D; 
 
     window.addEventListener("load", handleLoad);
 
 
     function handleLoad(_event: Event): void {
+        let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas"); 
+        if (!canvas)
+            return; 
+        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d"); 
+
         
         drawBackground();
+        drawHumanCell({x: 350, y: 170},  {x: 250, y: 250});  
 
     }
 
@@ -39,6 +45,37 @@ namespace coronaVirus {
 
         crc2.fillStyle = <CanvasRenderingContext2D>crc2.createPattern(pattern.canvas, "repeat");
         crc2.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function drawHumanCell (_position: Vector, _size: Vector ): void {
+
+        console.log("Cloud", _position, _size);
+        let nParticles: number = 30; 
+        let radiusParticle: number = 60; 
+        let particle: Path2D = new Path2D(); 
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle); 
+
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI); 
+        gradient.addColorStop(0, "HSLA(70%, 130%, 180%, 0.5)"); 
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+
+        crc2.save(); 
+
+        crc2.translate(_position.x, _position.y); 
+
+        crc2.fillStyle = gradient;
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+
+            crc2.save(); 
+            let x: number = (Math.random() - 0.5) * _size.x; 
+            let y: number = -(Math.random() * _size.y); 
+            crc2.translate(x, y); 
+            crc2.fill(particle); 
+            crc2.restore(); 
+        }
+        crc2.restore();
+
     }
 
 
